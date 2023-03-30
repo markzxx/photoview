dc4a99eeb138fc7f4862f60923668b1e0cc59d1d
zengxinxun <zengxinxun@bytedance.com>
2023-03-30 12:50:47 +0800

    fix

--- api/scanner/scanner_media.go
+++ api/scanner/scanner_media.go
@@ -78,11 +78,9 @@ func ScanMedia(tx *gorm.DB, mediaPath string, albumId int, cache *scanner_cache.
 func ProcessSingleMedia(db *gorm.DB, media *models.Media, album *models.Album) error {
 	album_cache := scanner_cache.MakeAlbumCache()
 
-	if album == nil {
-		album = &models.Album{}
-		if err := db.Model(media).Association("Album").Find(&album); err != nil {
-			return err
-		}
+	album = &models.Album{}
+	if err := db.Model(media).Association("Album").Find(&album); err != nil {
+		return err
 	}
 
 	media_data := media_encoding.NewEncodeMediaData(media)
